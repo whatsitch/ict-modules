275d77075a7721735f0ce4b21ced9ecf3ec52b56
whatsitch <marco@whatsit.ch>
2021-06-13 21:27:28 +0200

    removed comments

--- Module 121/src/classes/LED.h
+++ Module 121/src/classes/LED.h
@@ -8,17 +8,11 @@ class LED {
   public:
     LED() {}
     LED(byte pin, Colour colour) {
-      // Use 'this->' to make the difference between the
-      // 'pin' attribute of the class and the 
-      // local variable 'pin' created from the parameter.
       this->pin = pin;
       init();
     }
     void init() {
       pinMode(this->pin, OUTPUT);
-      // Always try to avoid duplicate code.
-      // Instead of writing digitalWrite(pin, LOW) here,
-      // call the function off() which already does that
       off();
     }
     void on() {
