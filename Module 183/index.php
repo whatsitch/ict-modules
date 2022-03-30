f67994273c9ac374fb35b5925f1286ec58f1c7d7
Marco <marco@whatsit.ch>
2022-03-30 23:36:45 +0200

    form test

--- Module 183/index.php
+++ Module 183/index.php
@@ -5,27 +5,98 @@
  *https://formr.github.io/
  * https://github.com/blackbelt/php-validation/blob/master/Validator.php
  */
-class Validator {
+
+class Validator
+{
     private $messages = array();
     private $errors = array();
     private $fields = array();
     private $rules = array();
-    private function setRule() {
+
+    private function setRule()
+    {
 
     }
 }
+
 /*----- TODO: import from redaxo -----*/
-class Form {
 
-    public function addTextField($name, $value = null, array $attributes = []) {
+enum InputType: string
+{
+    case TEXT = "text";
+    case TEXTAREA = "textarea";
+    case DATE = "date";
+    case EMAIL = "email";
+    case URL = "url";
+    case PHONE_NUMBER = "tel";
+    case NUMBER = "number";
+    case SUBMIT = "submit";
+    case RESET = "reset";
+    case HIDDEN = "hidden";
+}
+
+class Field
+{
+    private InputType $type;
+    private string $label;
+
+    public function setLabel(string $label)
+    {
+        $this->label = $label;
+    }
+}
+
+class Form
+{
+    private $method = "POST";
+    private array $fields;
+
+    public function addTextField($name, $value = null, array $attributes = [])
+    {
+        $field = $this->addInputField(InputType::TEXT, $name, $value, $attributes);
+        $this->addElement($field);
 
     }
 
-    public function addSelectField($name, $value = null, array $attributes = []) {
+    private function addElement($element)
+    {
+        array_push($this->fields, $element);
+    }
+
+    public function addTextAreaField(string $name, $value = null, array $attributes = [])
+    {
+
+        $field = $this->addInputField(InputType::TEXTAREA, $name, $value, $attributes);
+        $this->addElement($field);
+    }
+
+    public function addSelectField(string $name, $value = null, array $attributes = [])
+    {
 
     }
 
-    public function get() {
+    private function addInputField(InputType $type, $name, $value = null, array $attributes = [], $addElement = true)
+    {
+        $attributes['type'] = $type;
+        return $this->addField('input', $name, $value, $attributes, $addElement);
+    }
+
+    private function addField(InputType $type, $name, $value = null, array $attributes = [], $addElement = true)
+    {
+        // $element = $this->createElement($tag, $name, $value, $attributes);
+
+        /* if ($addElement) {
+             $this->addElement($element);
+             return $element;
+         }
+
+         return $element;*/
+        return $field;
+    }
+
+
+    public function get()
+    {
 
     }
 
@@ -41,15 +112,16 @@ enum ActionType: string
 $output = '';
 
 
-if(isset($_POST['action'])) {
+if (isset($_POST['action'])) {
 
 
     $action = ActionType::from($_POST['action']);
 
-    $output = $_POST['input'];
+    $output = $_POST['action'];
+    var_dump($_POST);
 
 
-    if($action === ActionType::SAVE) {
+    if ($action === ActionType::SAVE) {
         var_dump("SAVE");
     }
 
@@ -74,7 +146,8 @@ if(isset($_POST['action'])) {
 
     <h1><?= $output; ?> </h1>
 
-    <label>Eingabefeld <input name="input" value="..."></label>
+    <label for="input">Eingabefeld</label>
+    <input id="input" name="test" value="..."/>
     <button name="action" value="save" type="submit">abschicken</button>
     <br/>
     <button name="action" value="delete" type="submit">Eingabefeld löschen und abschicken</button>
