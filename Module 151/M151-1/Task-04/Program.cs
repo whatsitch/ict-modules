c57a537ff3306d55e96f3de15c55613f5ec30ecb
Marco <marco@whatsit.ch>
2022-05-02 22:09:43 +0200

    task 4

---
+++ Module 151/M151-1/Task-04/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data.SqlClient;
+
+
+
+try
+{
+    using (SqlConnection conn = new SqlConnection())
+    {
+        SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder()
+        {
+            DataSource = @"(localdb)\MSSQLLocalDB",
+            InitialCatalog = "Northwind",
+            IntegratedSecurity = true,
+            Pooling = true
+        };
+
+        conn.ConnectionString = builder.ConnectionString;
+
+
+        for (int i = 0; i < 10; i++)
+        {
+            conn.Open();
+            Console.WriteLine("connection {0} opened", i);
+            conn.Close(); 
+            Console.WriteLine("connection {0} closed", i);
+
+        }
+
+
+
+    };
+
+}
+catch (Exception e)
+{
+    Console.WriteLine("database not found");
+}
+
+
+Console.ReadLine();
