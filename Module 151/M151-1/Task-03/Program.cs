c3005f59bf47c6ccd54fbb4b27228ecbaac60aff
Marco <marco@whatsit.ch>
2022-05-02 22:05:26 +0200

    task 3

---
+++ Module 151/M151-1/Task-03/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data.SqlClient;
+
+
+
+try
+{
+    using (SqlConnection conn = new SqlConnection())
+    {
+        SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder()
+        {
+            DataSource = @"(localdb)\MSSQLLocalDB",
+            InitialCatalog = "Northwind",
+            IntegratedSecurity = true,
+        };
+
+        conn.ConnectionString = builder.ConnectionString;
+
+    
+        conn.Open();
+        Console.WriteLine("success");
+        conn.Close(); 
+    };
+
+}
+catch (Exception e)
+{
+    Console.WriteLine("database not found");
+}
+
+
+Console.ReadLine();
