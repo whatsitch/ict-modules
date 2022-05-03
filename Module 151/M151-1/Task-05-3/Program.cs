ad8e2b89f4e816a4cd9089703f2b565aad88df09
whatsitch <marco@whatsit.ch>
2022-05-03 14:42:25 +0200

    Task 05-3

---
+++ Module 151/M151-1/Task-05-3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data.SqlClient;
+
+
+try
+{
+    using (SqlConnection conn = new SqlConnection())
+    {
+        SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder()
+        {
+            DataSource = @"(localdb)\MSSQLLocalDB",
+            InitialCatalog = "Northwind",
+            IntegratedSecurity = true,
+        };
+        conn.ConnectionString = builder.ConnectionString;
+
+        const string sql = "DELETE FROM PRODUCTS WHERE ProductName = 'Schweizer Käse' ";
+        SqlCommand command = new SqlCommand(sql, conn);
+
+        conn.Open();
+        if (command.ExecuteNonQuery() > 0)
+            Console.WriteLine("dataset successfully deleted");
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine("database not found");
+    Console.WriteLine(e);
+}
+
+
+Console.ReadLine();
