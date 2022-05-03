f6e9384f9caa48601d8babb44184c25240d0494d
whatsitch <marco@whatsit.ch>
2022-05-03 14:12:00 +0200

    Task 05 added

---
+++ Module 151/M151-1/Task-05/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data.SqlClient;
+
+
+try
+{
+    using (SqlConnection conn = new SqlConnection())
+    {
+        SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder()
+        {
+            DataSource = @"(localdb)\MSSQLLocalDB",
+            InitialCatalog = "Northwind",
+            IntegratedSecurity = true,
+        };
+        conn.ConnectionString = builder.ConnectionString;
+        
+        const string sql = "UPDATE PRODUCTS SET ProductName = 'Sojasauce' WHERE ProductName = 'Chai'";
+
+        SqlCommand command = new SqlCommand(sql, conn);
+
+        conn.Open();
+        if (command.ExecuteNonQuery() > 0)
+            Console.WriteLine("dataset successfully updated");
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine("database not found");
+    Console.WriteLine(e);
+}
+
+
+Console.ReadLine();
