3f0cc55a99316721ce9be989c0fe2ed059920050
whatsitch <marco@whatsit.ch>
2022-05-03 14:18:09 +0200

    Task 05-02 added

---
+++ Module 151/M151-1/Task-05-2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data.SqlClient;
+
+
+try
+{
+    using (SqlConnection conn = new SqlConnection())
+    {
+        SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder()
+        {
+            DataSource = @"(localdb)\MSSQLLocalDB",
+            InitialCatalog = "Northwind",
+            IntegratedSecurity = true,
+        };
+        conn.ConnectionString = builder.ConnectionString;
+
+        const string sql = "INSERT INTO PRODUCTS(ProductName, Discontinued) Values('Schweizer Käse', 0) ";
+        SqlCommand command = new SqlCommand(sql, conn);
+
+        conn.Open();
+        if (command.ExecuteNonQuery() > 0)
+            Console.WriteLine("dataset successfully inserted");
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine("database not found");
+    Console.WriteLine(e);
+}
+
+
+Console.ReadLine();
